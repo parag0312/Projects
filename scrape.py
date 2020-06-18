ed5daf5a17836bc6e9cdca8dd0332ff5b97dd921
parag0312 <65803427+parag0312@users.noreply.github.com>
2020-06-18 20:12:48 +0530

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+
+soup =  BeautifulSoup(res.text,'html.parser')
+soup2 = BeautifulSoup(res2.text,'html.parser')
+
+links = soup.select('.storylink')
+links2 = soup2.select('.storylink')
+
+subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+mega_link = links + links2
+mega_subtext = subtext + subtext2
+
+def sorted_stories_by_votes(hn):
+	return sorted(hn, key = lambda k:k['votes'],reverse = True)
+
+
+def create_custom_hn(links,subtext):
+	hn = []
+	for index,item in enumerate(links):
+		title = item.getText()
+		href = item.get('href',None)
+		vote = subtext[index].select('.score')
+		if len(vote):
+			points = int(vote[0].getText().replace(' points',''))
+			if points > 99:
+				hn.append({'title': title,'link' : href,'votes': points})
+	return sorted_stories_by_votes (hn)		
+
+
+pprint.pprint(create_custom_hn(mega_link,mega_subtext))
+
+
+
